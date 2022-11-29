1b8edc36ceb50e237c0945dc21c9408cb5f98515
Renan Corredato <renan.lopes@mblabs.com.br>
2022-11-28 21:05:36 -0300

    frist recycler view

--- app/src/main/java/com/renancorredato/recyclerview/MainActivity.kt
+++ app/src/main/java/com/renancorredato/recyclerview/MainActivity.kt
@@ -2,10 +2,21 @@ package com.renancorredato.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.renancorredato.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val users = mutableListOf<String>()
+        repeat(1000){
+            users.add("Renan Corredato $it" )
+        }
+        binding.rvUser.adapter = UserAdapter(users)
     }
 }
